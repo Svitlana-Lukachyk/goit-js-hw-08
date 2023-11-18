df3e670666aeebd7acff0de33c42da2e42548e56
Svitlana-Lukachyk <svitlanalukachyk@gmail.com>
2023-11-18 12:30:38 +0200

    update task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,19 +1,32 @@
 import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
 form.addEventListener('input', throttle(onFormData, 500));
 form.addEventListener('submit', onFormSubmit);
 
+populateTextarea();
 const formData = {};
 
 function onFormData(evt) {
     formData[evt.target.name] = evt.target.value;
-    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 };
 
 function onFormSubmit(evt) {
     evt.preventDefault();
-    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
-    localStorage.removeItem('feedback-form-state');
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+    localStorage.removeItem(STORAGE_KEY);
     evt.currentTarget.reset();
+};
+
+function populateTextarea() {
+    const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    const email = document.querySelector('.feedback-form input');
+    const message = document.querySelector('.feedback-form textarea');
+
+    if(savedMessage) {
+    email.value = savedMessage.email || '';
+    message.value = savedMessage.message || '';
+    }
 };
